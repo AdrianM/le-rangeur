93a074af07115ebf3e50073d99a7a13e43a6a1c6
Adrian Morgenegg <adrian@software.catering>
2025-02-20 10:20:25 +0100

    #0 revalidate after 20min, fix endless caching

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,27 +1,29 @@
-import axios from 'axios';
 import * as cheerio from 'cheerio';
 import { Player } from './definitions';
-import fs from 'fs';
+
 
 export async function fetchPlayers(): Promise<Player[]> {
   try {
+    console.log('Server > fetchPlayers > start');
     // TODO AMO: get this from the browser.
     const sessionCookie = 'ASP.NET_SessionId=qwu5upju2tjmzqgb2zigdw1i; st=l=2055&exp=46067.4497164815&c=1&cp=51; _ga_MSFCJGD4YS=GS1.1.1739526455.1.0.1739526455.0.0.0; _ga=GA1.1.196022657.1739526456';
     
     // const response = await axios.get('http://localhost:3000/tmp-swiss-badminton-source.html');
-    const response = await axios.get('https://www.swiss-badminton.ch/ranking/category.aspx?rid=209&category=2792&C2792FTYAF=0&C2792FTYAT=0&C2792FOG_3_F2048=79371&C2792RFN=&p=1&ps=100', {
+    const response = await fetch('https://www.swiss-badminton.ch/ranking/category.aspx?rid=209&category=2792&C2792FTYAF=0&C2792FTYAT=0&C2792FOG_3_F2048=79371&C2792RFN=&p=1&ps=100', {
       headers: {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Cookie': sessionCookie
       },
-      withCredentials: true
+      credentials: 'include',
+      next: {
+        revalidate: 1200 // 20 minutes
+      }
     });
 
-    console.log('Server > Data loaded');
-    fs.writeFileSync('debug-amo-fetch-players-1.html', response.data);
-
-    const html = response.data;
+    
+    const html = await response.text();
+    // fs.writeFileSync('debug-amo-fetch-players-1.html', html); // for debugging
     const $ = cheerio.load(html);
     
     const players: Player[] = [];
@@ -66,7 +68,7 @@ export async function fetchPlayers(): Promise<Player[]> {
         }
     });
     
-    // Sort by ranking
+    console.log(`Server > fetchPlayers > done. Found ${players.length} players, lastUpdatedDate: ${lastUpdatedDate}`);
     return players.sort((a, b) => a.rankingNr - b.rankingNr);
   } catch (error) {
     console.error('Error fetching players:', error);
